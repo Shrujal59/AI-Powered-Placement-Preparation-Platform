5c42ccabdde2b40a105fe2ff488111b29dce2f42
Shrujal Soni <sonishrujal448@gmail.com>
2026-06-16 14:56:02 +0530

    Implemented React login with JWT token storage

--- placement-platform/frontend/src/pages/Login.jsx
+++ placement-platform/frontend/src/pages/Login.jsx
@@ -1,7 +1,63 @@
-export default function Login() {
+import { useState } from "react";
+import api from "../api/axios";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+   const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+    const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/accounts/login/", formData);
+
+      localStorage.setItem("access", response.data.access);
+      localStorage.setItem("refresh", response.data.refresh);
+
+      alert("Login Successful");
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+      alert("Login Failed");
+    }
+  };
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
+
+export default Login;
